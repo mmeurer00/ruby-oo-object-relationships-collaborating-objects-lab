2799d1510e0f85d08b048b87694cefed72c55dfa
Maxine <maxinemeurer@yahoo.com>
2021-02-16 19:42:50 -0500

    commit tree d9dae5131766c2aeecc1783b19f880544cbb80dd

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+    attr_accessor :artist, :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
+    end
+
+    def self.new_by_filename(filename)
+        artist, song = filename.split(" - ")
+        new_song = self.new(song)
+        new_song.artist_name = artist
+        new_song
+    end
+
+    #def artist_name=(name)
+        #if (self.artist.nil?)
+            #self.artist = Artist.new(name)
+        #else
+            #self.artist.name = name
+        #end
+    #end
+end
